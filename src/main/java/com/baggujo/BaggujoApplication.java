9d9e5b2245cb85f93d8ecf93da67d40e09da1a8d
rjsah09 <rjsah09@naver.com>
2024-07-02 17:34:09 +0900

    빈 프로젝트 생성

---
+++ src/main/java/com/baggujo/BaggujoApplication.java
@@ -0,0 +1,13 @@
+package com.baggujo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaggujoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaggujoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baggujo/BaggujoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baggujo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaggujoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
