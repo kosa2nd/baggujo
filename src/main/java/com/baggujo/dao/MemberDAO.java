954cb2f7c7a22a8ada05a1cf3a527c4508c31351
rjsah09 <rjsah09@naver.com>
2024-07-03 14:10:17 +0900

    [MemberDAO] findMemberByEmail 메서드 추가

--- src/main/java/com/baggujo/dao/MemberDAO.java
+++ src/main/java/com/baggujo/dao/MemberDAO.java
@@ -1,4 +1,9 @@
 package com.baggujo.dao;
 
+import com.baggujo.dto.MemberDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface MemberDAO {
+    MemberDTO findMemberByEmail(String email);
 }
