af4aad3be03ff2ce2a403a87edaadfb4180d0382
rjsah09 <rjsah09@naver.com>
2024-07-03 11:36:00 +0900

    Member 관련 클래스 생성

---
+++ src/main/java/com/baggujo/dao/MemberDAO.java
@@ -0,0 +1,4 @@
+package com.baggujo.dao;
+
+public interface MemberDAO {
+}
---
+++ src/main/java/com/baggujo/service/MemberService.java
@@ -0,0 +1,7 @@
+package com.baggujo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+}
