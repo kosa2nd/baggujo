12e82c2fcd6d6aeed3965a1befd2a9cadc59e821
이연수 <dldustn2040@gmail.com>
2024-07-04 16:20:01 +0900

    insert 페이지 연결

--- src/main/java/com/baggujo/controller/ItemController.java
+++ src/main/java/com/baggujo/controller/ItemController.java
@@ -3,11 +3,18 @@ package com.baggujo.controller;
 import com.baggujo.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/item")
 public class ItemController {
 
     @Autowired
     private ItemService itemService;
 
+    @GetMapping("/insert")
+    public String insert() {
+        return "item/insert";
+    }
 }
--- src/main/java/com/baggujo/dao/ItemDAO.java
+++ src/main/java/com/baggujo/dao/ItemDAO.java
@@ -1,8 +1,10 @@
 package com.baggujo.dao;
 
+import com.baggujo.dto.ItemInsertDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ItemDAO {
+    ItemInsertDTO insertItem(ItemInsertDTO item);
 
 }
