f734d7d6c404c14590b083e8a141a0ea884d6c35
이연수 <dldustn2040@gmail.com>
2024-07-04 12:16:40 +0900

    post controller 생성

---
+++ src/main/java/com/baggujo/controller/PostController.java
@@ -0,0 +1,14 @@
+package com.baggujo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PostController {
+
+    @GetMapping("/post")
+    public String post() {
+        return "post/post";
+    }
+
+}
