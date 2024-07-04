074401ff95ea5b4bf7a755db50153904367be832
이연수 <dldustn2040@gmail.com>
2024-07-04 21:33:51 +0900

    게시글 작성 페이지 생성

--- src/main/java/com/baggujo/controller/ItemController.java
+++ src/main/java/com/baggujo/controller/ItemController.java
@@ -1,10 +1,17 @@
 package com.baggujo.controller;
 
+import com.baggujo.dto.ItemInsertDTO;
 import com.baggujo.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/item")
@@ -17,4 +24,15 @@ public class ItemController {
     public String insert() {
         return "item/insert";
     }
+
+    @PostMapping("/insert")
+    public String submitPost(@ModelAttribute ItemInsertDTO itemInsertDTO) {
+        try {
+            ItemInsertDTO insertedItem = itemService.insertItem(itemInsertDTO);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "redirect:/item/list";
+    }
+
 }
