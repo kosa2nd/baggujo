fc639b3b530adf6864672fae94f930a13e0b80c2
rjsah09 <rjsah09@naver.com>
2024-07-04 16:28:57 +0900

    ItemService.insetItem() 메서드 생성

--- src/main/java/com/baggujo/dao/ItemDAO.java
+++ src/main/java/com/baggujo/dao/ItemDAO.java
@@ -3,8 +3,10 @@ package com.baggujo.dao;
 import com.baggujo.dto.ItemInsertDTO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.sql.SQLException;
+
 @Mapper
 public interface ItemDAO {
-    ItemInsertDTO insertItem(ItemInsertDTO item);
+    long insertItem(ItemInsertDTO itemInsertDTO) throws SQLException;
 
 }
--- src/main/java/com/baggujo/service/ItemService.java
+++ src/main/java/com/baggujo/service/ItemService.java
@@ -2,8 +2,11 @@ package com.baggujo.service;
 
 import com.baggujo.dao.ItemDAO;
 import com.baggujo.dao.ItemImageDAO;
+import com.baggujo.dto.ItemInsertDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
 
 @Service
 public class ItemService {
@@ -12,4 +15,8 @@ public class ItemService {
     private ItemDAO itemDAO;
     @Autowired
     private ItemImageDAO itemImageDAO;
+
+    public long insertItem(ItemInsertDTO itemInsertDTO) throws SQLException {
+        return itemDAO.insertItem(itemInsertDTO);  //id 값 반환
+    }
 }
