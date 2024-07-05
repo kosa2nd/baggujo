86e92c78028908e42c19362022106a7bc6351d0d
rjsah09 <rjsah09@naver.com>
2024-07-05 18:11:10 +0900

    작업내용 저장

--- src/main/java/com/baggujo/controller/rest/RestItemController.java
+++ src/main/java/com/baggujo/controller/rest/RestItemController.java
@@ -1,12 +1,37 @@
 package com.baggujo.controller.rest;
 
+import com.baggujo.dto.ItemPreviewDTO;
 import com.baggujo.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.SQLException;
+import java.util.List;
+
 @RestController
+@RequestMapping("/item")
 public class RestItemController {
     @Autowired
     private ItemService itemService;
+    @Value("${com.baggujo.paging.offset}")
+    private int offset;
+
+    @GetMapping("/list")
+    public ResponseEntity<List<ItemPreviewDTO>> getItems(@RequestBody int itemId) {
+        List<ItemPreviewDTO> itemPreviewDTOS;
+        try {
+            itemPreviewDTOS = itemService.getItemPreviews(itemId, offset);
+        } catch (SQLException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(itemPreviewDTOS, HttpStatus.OK);
+    }
 
 }
