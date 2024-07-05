46e9e5d3e9972c20b860a752cdf2e8c75f208ef1
이연수 <dldustn2040@gmail.com>
2024-07-05 17:10:06 +0900

    충돌해결

--- src/main/java/com/baggujo/dao/ItemDAO.java
+++ src/main/java/com/baggujo/dao/ItemDAO.java
@@ -3,7 +3,6 @@ package com.baggujo.dao;
 import com.baggujo.dto.CategoryDTO;
 import com.baggujo.dto.ItemDetailDTO;
 import com.baggujo.dto.ItemInsertDTO;
-import com.baggujo.dto.ItemPreviewDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.sql.SQLException;
@@ -15,6 +14,4 @@ public interface ItemDAO {
 
     ItemDetailDTO getItemDetailById(long id);
     List<CategoryDTO> getCategories();
-
-    List<ItemPreviewDTO> getItemPreviews(int page, int offset) throws SQLException;
 }
