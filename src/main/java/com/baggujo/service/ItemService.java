6f108fd49666e1c4bda78bae89299a8c9727d636
rjsah09 <rjsah09@naver.com>
2024-07-05 09:31:04 +0900

    임시로 이미지 저장 로직 주석처리

--- src/main/java/com/baggujo/service/ItemService.java
+++ src/main/java/com/baggujo/service/ItemService.java
@@ -38,7 +38,7 @@ public class ItemService {
     public long insertItem(ItemInsertDTO itemInsertDTO) throws SQLException, FileNotFoundException {
         itemDAO.insertItem(itemInsertDTO);
         long id = itemInsertDTO.getId();
-
+        /*
         List<ItemImageInsertDTO> itemInsertDTOList = new ArrayList<>();
         LocalDate today = LocalDate.now();
         String path = makeFolder();
@@ -69,7 +69,7 @@ public class ItemService {
 
             int actualUpload = itemImageDAO.insertItemImages(itemInsertDTOList);
         }
-
+        */
         return id;
     }
 
