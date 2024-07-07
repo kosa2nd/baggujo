908ad153e71be29f25e0064127b51cf728bd42a8
rjsah09 <rjsah09@naver.com>
2024-07-08 02:10:48 +0900

    물품 리스트 조회 검색 조건 추가

--- src/main/java/com/baggujo/controller/rest/RestItemController.java
+++ src/main/java/com/baggujo/controller/rest/RestItemController.java
@@ -1,15 +1,13 @@
 package com.baggujo.controller.rest;
 
 import com.baggujo.dto.ItemPreviewDTO;
+import com.baggujo.dto.enums.ItemStatus;
 import com.baggujo.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -23,10 +21,10 @@ public class RestItemController {
     private int offset;
 
     @GetMapping("/list")
-    public ResponseEntity<List<ItemPreviewDTO>> getItems(@RequestBody int itemId) {
+    public ResponseEntity<List<ItemPreviewDTO>> getItems(@RequestBody int itemId, @RequestParam ItemStatus itemStatus, @RequestParam(defaultValue = "0") int itemCategoryId, @RequestParam(defaultValue = "true") boolean exceptTraded, @RequestParam String keyword) {
         List<ItemPreviewDTO> itemPreviewDTOS;
         try {
-            itemPreviewDTOS = itemService.getItemPreviews(itemId, offset);
+            itemPreviewDTOS = itemService.getItemPreviews(itemId, offset, itemStatus, itemCategoryId, exceptTraded, keyword);
         } catch (SQLException e) {
             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
