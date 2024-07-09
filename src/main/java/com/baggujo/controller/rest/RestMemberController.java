6d54429dc606654ebff9dacdc5e712a362612eff
vnfam <vnfam0626@gmail.com>
2024-07-09 15:36:30 +0900

    회원 중복 체크 RestController

--- src/main/java/com/baggujo/controller/rest/RestMemberController.java
+++ src/main/java/com/baggujo/controller/rest/RestMemberController.java
@@ -1,7 +1,29 @@
 package com.baggujo.controller.rest;
 
+import com.baggujo.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.SQLException;
+
 @RestController
+@RequestMapping("/mamber")
 public class RestMemberController {
+
+    @Autowired
+    private MemberService memberService;
+
+    public ResponseEntity<Boolean> dupliAccount(@RequestParam String email) {
+        try{
+            boolean duplicated = memberService.getEmailDuplicated(email);
+            return new ResponseEntity<>(duplicated, HttpStatus.OK);
+        } catch (SQLException e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- src/test/java/com/baggujo/service/FavoriteServiceTest.java
+++ src/test/java/com/baggujo/service/FavoriteServiceTest.java
@@ -12,16 +12,4 @@ import java.sql.SQLException;
 public class FavoriteServiceTest {
     @Autowired
     private FavoriteService favoriteService;
-
-    @Test
-    @DisplayName("즐겨찾기 토글 테스트")
-    public void 즐겨찾기토글테스트() throws SQLException {
-        long memberId = 1L;
-        long itemId = 91L;
-
-        boolean result1 = favoriteService.toggleFavorite(memberId, itemId);
-        boolean result2 = favoriteService.toggleFavorite(memberId, itemId);
-        Assertions.assertTrue(result1 == !result2);
-    }
-
 }
