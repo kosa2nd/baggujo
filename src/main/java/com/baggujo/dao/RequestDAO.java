396881b86240a5bdae60e54068fddf74ea00b2d4
rjsah09 <rjsah09@naver.com>
2024-07-10 10:23:07 +0900

    요청 수락 기능 제작

--- src/main/java/com/baggujo/dao/RequestDAO.java
+++ src/main/java/com/baggujo/dao/RequestDAO.java
@@ -2,6 +2,7 @@ package com.baggujo.dao;
 
 import com.baggujo.dto.RequestDTO;
 import com.baggujo.dto.RequestInsertDTO;
+import com.baggujo.dto.enums.RequestStatus;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.sql.SQLException;
@@ -12,5 +13,6 @@ public interface RequestDAO {
 
     void insertRequest(RequestInsertDTO requestInsertDTO) throws SQLException;
     List<RequestDTO> getRequestByMemberId(long memberId, long lastRequestId, Boolean request, long offset) throws SQLException;
+    int updateRequestStatus(long requestId, RequestStatus requestStatus);
 
 }
---
+++ src/main/java/com/baggujo/service/TradeService.java
@@ -0,0 +1,66 @@
+package com.baggujo.service;
+
+import com.baggujo.dao.ItemDAO;
+import com.baggujo.dao.RequestDAO;
+import com.baggujo.dao.TradeDAO;
+import com.baggujo.dto.RequestDTO;
+import com.baggujo.dto.RequestInsertDTO;
+import com.baggujo.dto.TradeInsertDTO;
+import com.baggujo.dto.enums.ItemStatus;
+import com.baggujo.dto.enums.RequestStatus;
+import org.apache.coyote.BadRequestException;
+import org.apache.ibatis.jdbc.SQL;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Service
+public class TradeService {
+
+    @Autowired
+    RequestDAO requestDAO;
+    @Autowired
+    ItemDAO itemDAO;
+    @Autowired
+    TradeDAO tradeDAO;
+
+    public void insertRequest(RequestInsertDTO requestInsertDTO) throws SQLException, BadRequestException {
+        if (itemDAO.getItemStatusById(requestInsertDTO.getRequestItemId()) != ItemStatus.WAITING
+            || itemDAO.getItemStatusById(requestInsertDTO.getResponseItemId()) != ItemStatus.WAITING) {
+            throw new BadRequestException("거래를 요청할 수 없습니다");
+        }
+
+        requestDAO.insertRequest(requestInsertDTO);
+    }
+
+    public List<RequestDTO> getAvailableRequestsByMemberId(long memberId, long lastRequestId, Boolean request, long offset) throws SQLException {
+        return requestDAO.getRequestByMemberId(memberId, lastRequestId, request, offset);
+    }
+
+    @Transactional
+    public long acceptRequest(long requestId) throws SQLException {
+        //물품 상태 변경
+        if (itemDAO.updateItemStatusByRequestId(requestId, ItemStatus.TRADING) != 2 ) {
+            throw new SQLException("요청을 처리할 수 없습니다");
+        }
+
+        //요청 상태 변경
+        if (requestDAO.updateRequestStatus(requestId, RequestStatus.ACCEPTED) != 1) {
+            throw new SQLException("요청을 처리할 수 없습니다");
+        }
+
+        //거래 생성
+        TradeInsertDTO tradeInsertDTO = new TradeInsertDTO(requestId);
+        if (tradeDAO.insertTradeByRequestId(tradeInsertDTO) != 1) {
+            throw new SQLException("요청을 처리할 수 없습니다");
+        }
+
+        //거래 아이디 반환
+        return tradeInsertDTO.getId();
+    }
+
+
+}
