16cbf8ca3adffa15090e72e5de0af878f0cc1828
rjsah09 <rjsah09@naver.com>
2024-07-16 15:51:14 +0900

    버그 수정

--- src/main/java/com/baggujo/controller/rest/RestNotificationController.java
+++ src/main/java/com/baggujo/controller/rest/RestNotificationController.java
@@ -1,26 +1,55 @@
 package com.baggujo.controller.rest;
 
+import com.baggujo.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/notification")
 public class RestNotificationController {
 
+    @Autowired
+    NotificationService notificationService;
+    @Value("${com.baggujo.paging.offset}")
+    private int offset;
+
+    //채팅방 입장하거나 채팅 수신시 관련 채팅 알림 모두 지우기
     @PostMapping("/removeAllChat")
-    public ResponseEntity<Boolean> removeAll(@RequestParam long memberId, long tradeId) {
+    public ResponseEntity<Boolean> removeAll(@RequestParam long memberId, @RequestParam long tradeId) {
+        try {
+            notificationService.removeAllChat(memberId, tradeId);
+        } catch (Exception e) {
+            return new ResponseEntity<Boolean>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
         return new ResponseEntity<Boolean>(true, HttpStatus.OK);
     }
 
-    @PostMapping("/update")
-    public ResponseEntity<Boolean> update() {
-
-        return new ResponseEntity<Boolean>(true, HttpStatus.OK);
-    }
-
+    //알림
+//    @PostMapping("/update")
+//    public ResponseEntity<boolean> update() {
+//
+//            long notificationId = -1;
+//            if (!dtos.isEmpty()) {
+//                notificationId = dtos.get(dtos.size() - 1).getId();
+//            } else {
+//                map.put("finished", true);
+//            }
+//
+//            map.put("lastNotificationId", notificationId);
+//        } catch (Exception e) {
+//            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//        return new ResponseEntity<>(map, HttpStatus.OK);
+//    }
 
 }
--- src/main/java/com/baggujo/dao/NotificationDAO.java
+++ src/main/java/com/baggujo/dao/NotificationDAO.java
@@ -1,5 +1,6 @@
 package com.baggujo.dao;
 
+import com.baggujo.dto.NotificationDetailDTO;
 import com.baggujo.dto.NotificationInsertDTO;
 import com.baggujo.dto.enums.NotificationStatus;
 import com.baggujo.dto.enums.TradeStatus;
@@ -7,13 +8,14 @@ import org.apache.ibatis.annotations.Mapper;
 
 import javax.management.remote.TargetedNotification;
 import java.sql.SQLException;
+import java.util.List;
 
 @Mapper
 public interface NotificationDAO {
     int insertNotification(NotificationInsertDTO notificationInsertDTO);       //일반적인 알림 생성
     void insertRequestCancelNotiByRequestId(long requestId) throws SQLException;
     void insertResponseCancelNotiByRequestId(long requestId) throws SQLException;
-    void updateNotificationById(long notificationId, NotificationStatus notificationStatus);
+    int updateNotificationById(long notificationId, NotificationStatus notificationStatus);
     void updateNotificationByMemberIdAndTradeId(long memberId, long tradeId, NotificationStatus notificationStatus);
-    //void getNotifications(long lastNotificationId, int offset);
+    List<NotificationDetailDTO> getNotifications(long memberId, long lastNotificationId, int offset);
 }
